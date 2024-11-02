3203bc7ac071beada7e451d4b4fd3e7d5be0f52f
Alejandro Sol <alejandro501@proton.me>
2024-11-03 00:13:18 +0100

    amass and robots

--- api/amass.sh
+++ api/amass.sh
@@ -3,7 +3,7 @@
 # Global Variables
 TARGET_ORGANIZATION=''
 ORGANIZATION_LIST=''
-AMASS_OUTPUT_DIR='amass'  
+AMASS_OUTPUT_DIR='amass'
 WORDLIST_FILE='wordlist.txt' # not used, later todo
 SUBDOMAIN_FILE="$AMASS_OUTPUT_DIR/subdomains"
 VISUALIZATION_FILE="$AMASS_OUTPUT_DIR/visualization.html"
@@ -27,11 +27,23 @@ usage() {
 get_params() {
     while [[ "$#" -gt 0 ]]; do
         case $1 in
-            -L|--list) ORGANIZATION_LIST="$2"; shift ;;
-            -D|--domain) TARGET_ORGANIZATION="$2"; shift ;;
-            -T|--tor) USE_TOR=true ;; 
-            -H|--help) usage; exit 0 ;;
-            *) echo "Unknown parameter: $1"; exit 1 ;;
+        -L | --list)
+            ORGANIZATION_LIST="$2"
+            shift
+            ;;
+        -D | --domain)
+            TARGET_ORGANIZATION="$2"
+            shift
+            ;;
+        -T | --tor) USE_TOR=true ;;
+        -H | --help)
+            usage
+            exit 0
+            ;;
+        *)
+            echo "Unknown parameter: $1"
+            exit 1
+            ;;
         esac
         shift
     done
@@ -39,7 +51,7 @@ get_params() {
 
 amass_operations() {
     mkdir -p "$AMASS_OUTPUT_DIR"
-    > "$SUBDOMAIN_FILE"  # Clear previous subdomain file contents
+    >"$SUBDOMAIN_FILE" # Clear previous subdomain file contents
 
     # Prefix with "torsocks" if Tor is enabled
     AMASS_CMD_PREFIX=""
@@ -53,18 +65,18 @@ amass_operations() {
         local domain_value=$3
 
         # Capture output, extract only the domain part, and append API-related subdomains to the subdomain file
-        $AMASS_CMD_PREFIX amass enum "$mode" "$domain_flag" "$domain_value" -dir "$AMASS_OUTPUT_DIR" -o amass_tmp \
-            | awk '{print $1}' | grep -i 'api' | anew "$SUBDOMAIN_FILE"
+        $AMASS_CMD_PREFIX amass enum "$mode" "$domain_flag" "$domain_value" -dir "$AMASS_OUTPUT_DIR" -o amass_tmp |
+            awk '{print $1}' | grep -i 'api' | anew "$SUBDOMAIN_FILE"
     }
 
     if [[ -n "$TARGET_ORGANIZATION" ]]; then
         # Run Amass commands for a single domain
         run_amass "-passive" "-d" "$TARGET_ORGANIZATION"
         run_amass "-active" "-d" "$TARGET_ORGANIZATION" -p 80,443
-        $AMASS_CMD_PREFIX amass intel -d "$TARGET_ORGANIZATION" -whois -dir "$AMASS_OUTPUT_DIR" \
-            | awk '{print $1}' | grep -i 'api' >> "$SUBDOMAIN_FILE"
+        $AMASS_CMD_PREFIX amass intel -d "$TARGET_ORGANIZATION" -whois -dir "$AMASS_OUTPUT_DIR" |
+            awk '{print $1}' | grep -i 'api' >>"$SUBDOMAIN_FILE"
         run_amass "-active -brute -w $WORDLIST_FILE" "-d" "$TARGET_ORGANIZATION"
-        
+
         # Generate visualization output
         $AMASS_CMD_PREFIX amass viz -enum -d3 -d "$TARGET_ORGANIZATION" -dir "$AMASS_OUTPUT_DIR" -o "$VISUALIZATION_FILE"
     fi
@@ -74,21 +86,20 @@ amass_operations() {
         while IFS= read -r org; do
             run_amass "-passive" "-df" "$ORGANIZATION_LIST"
             run_amass "-active" "-df" "$ORGANIZATION_LIST" -p 80,443
-            $AMASS_CMD_PREFIX amass intel -df "$ORGANIZATION_LIST" -whois -dir "$AMASS_OUTPUT_DIR" \
-                | awk '{print $1}' | grep -i 'api' >> "$SUBDOMAIN_FILE"
+            $AMASS_CMD_PREFIX amass intel -df "$ORGANIZATION_LIST" -whois -dir "$AMASS_OUTPUT_DIR" |
+                awk '{print $1}' | grep -i 'api' >>"$SUBDOMAIN_FILE"
             run_amass "-active -brute -w $WORDLIST_FILE" "-df" "$ORGANIZATION_LIST"
 
             # Generate visualization output for each domain list
             $AMASS_CMD_PREFIX amass viz -enum -d3 -df "$ORGANIZATION_LIST" -dir "$AMASS_OUTPUT_DIR" -o "$VISUALIZATION_FILE"
-        done < "$ORGANIZATION_LIST"
+        done <"$ORGANIZATION_LIST"
 
-        
     fi
 }
 
 main() {
     get_params "$@"
-    
+
     # Check if at least one of the parameters is provided
     if [[ -z "$TARGET_ORGANIZATION" && -z "$ORGANIZATION_LIST" ]]; then
         usage
